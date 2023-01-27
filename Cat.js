4e90ddc9e024095e181bb3ede4c682c13d9ae560
Ngan <hongngan03dn@gmail.com>
2023-01-27 15:41:17 +0700

    'today2023'

--- Cat.js
+++ Cat.js
@@ -1,9 +1,10 @@
-function Cat () {
-	this.stomach = [];
+function Cat() {
+    this.stomach = [];
+    // today is 27012023
 }
 
 Cat.prototype.eat = function(mouse) {
-	this.stomach.push(mouse);
+    this.stomach.push(mouse);
 };
 
 module.exports = Cat;
